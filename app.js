3cbd6361f786ae92bd339a8390f4279f4a1acd99
jamesnathanan <57458881+jamesnathanan@users.noreply.github.com>
2021-08-16 01:31:12 +0700

    update query that get from html form

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express")
 const https = require("https")
+const bodyParser = require("body-parser")
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
 
 const apiKey = "98aaf1debbf8806d60944a1dc9ffe751"
 
 app.get("/", (req, res) => {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Manchester&appid="+apiKey
+    res.sendFile(__dirname + '/index.html')
     
+})
+
+app.post("/", (req, res) => {
+    console.log('Post Received')
+    console.log(req.body.cityName)
+
+    const query = req.body.cityName
+    const unit = 'metric'
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+apiKey+"&units="+unit
+    const units = unit === 'metric' ? 'Celsius' : 'Kelvins'
     https.get(url, (response) => {
         console.log(response.statusCode)
 
@@ -22,7 +34,7 @@ app.get("/", (req, res) => {
             const icon = weatherData.weather[0].icon
             const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
             res.write("<h1>The weather in " + cityName + " today</h1>")
-            res.write(`<h2>The Temperature is ${temp} Kelvin and has ${des}</h2>`)
+            res.write(`<h2>The Temperature is ${temp} Degree ${units} and has ${des}</h2>`)
             res.write("<img src=" + imageURL + ">")
             res.send()
             //res.send("Hi")
@@ -34,3 +46,5 @@ app.get("/", (req, res) => {
 app.listen( 3000, () => {
     console.log('Server is running on port 3000')
 })
+
+
